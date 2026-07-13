360d69dc755452db706acb1fd3e5615de3b0c4c5
gauravinodgupta33-lang <gauravinodgupta33@gmail.com>
2026-07-13 11:54:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+// Function to generate the board
+function createBoard() {
+    const boardTrack = document.getElementById('board-track');
+    
+    // Loop 200 times to create 200 squares
+    for (let i = 1; i <= 200; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerText = i;
+        square.id = 'square-' + i;
+        
+        // Add click event for when you're ready for tasks
+        square.onclick = () => console.log("Clicked square " + i);
+        
+        boardTrack.appendChild(square);
+    }
+}
+
+// Run the function when the page loads
+window.onload = createBoard;
